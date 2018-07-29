4485c2a201a1d8e1c2cfe08d2caf3309d3b45518
jeffreykxiao <jeffrey@ascent.ai>
2018-07-30 01:09:04 +0900

    irisify by swappin ctrl with caps

--- keyboards/xd75/keymaps/jeffx/keymap.c
+++ keyboards/xd75/keymaps/jeffx/keymap.c
@@ -36,20 +36,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
  * | TAB    | Q      | W      | E      | R      | T      |        | VOLUP  |        | Y      | U      | I      | O      | P      | DEL    |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
- * | LCAPS  | A      | S      | D      | F      | G      | PREV   | PLAY   | NEXT   | H      | J      | K      | L      | ;      | ENTER  |
+ * | LCTRL  | A      | S      | D      | F      | G      | PREV   | PLAY   | NEXT   | H      | J      | K      | L      | ;      | ENTER  |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
  * | LSHIFT | Z      | X      | C      | V      | B      |        | VOLDN  |        | N      | M      | ,      | .      | /      | RSHIFT |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
- * | LCTRL  | OS     | ALT    | FN     | SPACE  | SPACE  | MUHEN  | KANA   | HENKAN | LOWER  | RAISE  | LEFT   | DOWN   | UP     | RIGHT  |
+ * | LCAPS  | OS     | ALT    | FN     | SPACE  | SPACE  | MUHEN  | KANA   | HENKAN | LOWER  | RAISE  | LEFT   | DOWN   | UP     | RIGHT  |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 
  [_QW] = { 
   { KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_GRV,  KC_MUTE, _______, KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_BSPC },
   { KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    _______, KC_VOLU, _______, KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_DEL  },
-  { KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_MPRV, KC_MPLY, KC_MNXT, KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_ENT  },
+  { KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_MPRV, KC_MPLY, KC_MNXT, KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_ENT  },
   { KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______, KC_VOLD, _______, KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT },
-  { KC_LCTL, KC_LGUI, KC_LALT, MO(_FN), KC_SPC,  KC_SPC,  KC_MHEN, KC_KANA, KC_HENK, MO(_LW),MO(_RS),KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT },
+  { KC_CAPS, KC_LGUI, KC_LALT, MO(_FN), KC_SPC,  KC_SPC,  KC_MHEN, KC_KANA, KC_HENK, MO(_LW),MO(_RS),KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT },
  },
 
   
