4be8ba4279efb997af3bd88dad32597e7ba0029d
Jeff Xiao <jeffrey@ascent.ai>
2018-07-30 15:53:37 +0900

    Thumbkeys aren't so good when you don't have a dedicated cluster

--- keyboards/xd75/keymaps/jeffx/keymap.c
+++ keyboards/xd75/keymaps/jeffx/keymap.c
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    _______, KC_VOLU, _______, KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_DEL  },
   { KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_MPRV, KC_MPLY, KC_MNXT, KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_ENT  },
   { KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______, KC_VOLD, _______, KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT },
-  { KC_CAPS, KC_LGUI, KC_LALT, MO(_FN), KC_MHEN, KC_SPC,  KC_SPC,  KC_KANA, MO(_LW), MO(_RS),KC_MHEN,KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT },
+  { KC_CAPS, KC_LGUI, KC_LALT, MO(_FN), KC_SPC,  KC_SPC,  KC_MHEN, KC_KANA, KC_HENK, MO(_LW),MO(_RS),KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT },
  },
 
   
