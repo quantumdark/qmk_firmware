339df605b2abf6dcf565e1143f0cb57b1f9fb294
jeffreykxiao <jeffrey@ascent.ai>
2018-10-26 20:10:27 +0900

    Add tapdance, gaming lock, multi-desktop

--- keyboards/dz60/keymaps/jeffx/keymap.c
+++ keyboards/dz60/keymaps/jeffx/keymap.c
@@ -1,6 +1,21 @@
 #include QMK_KEYBOARD_H
 
-#define ______ KC_TRNS
+#define ________ KC_TRNS
+#define _QW 0
+#define _GM 1
+#define _FN 2
+#define _ST 3
+
+enum {
+  TD_HOME= 0,
+  TD_GAME= 1,
+};
+
+//Tap Dance Definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [TD_HOME]  = ACTION_TAP_DANCE_DUAL_ROLE(KC_RALT, _QW),
+  [TD_GAME]  = ACTION_TAP_DANCE_DUAL_ROLE(KC_RGUI, _GM)
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -10,20 +25,42 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----------------------------------------------------------------------------------------+
    * | Tab    |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |  [  |  ]  |    \   |
    * |-----------------------------------------------------------------------------------------+
-   * | Caps    |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  |    Enter    |
+   * | FN      |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  |    Enter    |
    * |-----------------------------------------------------------------------------------------+
    * | Shift     |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  | RSh |  U  | DEL |
    * |-----------------------------------------------------------------------------------------+
-   * | Ctrl |  Cmd  |  Alt  |              Space                | RAlt | FN  |  L  |  D  |  R  |
+   * | Ctrl |  OS   |  Alt  |              Space                | RAlt | OS  |  L  |  D  |  R  |
    * `-----------------------------------------------------------------------------------------'
    */
 
   LAYOUT_directional(
-      KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, ______, KC_BSPC,
+      KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, ________, KC_BSPC,
       KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
-      KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
-      KC_LSFT,         KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_DEL,
-      KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_SPC, KC_SPC, KC_KANA, MO(1), KC_LEFT, KC_DOWN, KC_RIGHT
+      MO(_FN), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
+      KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_DEL,
+      KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_SPC, KC_SPC, TD(TD_GAME), MO(_ST), KC_LEFT, KC_DOWN, KC_RIGHT
+      ),
+
+  /* Qwerty Gaming Layer
+   * ,-----------------------------------------------------------------------------------------.
+   * | Esc |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |   Bkspc   |
+   * |-----------------------------------------------------------------------------------------+
+   * | Tab    |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |  [  |  ]  |    \   |
+   * |-----------------------------------------------------------------------------------------+
+   * | FN      |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  |    Enter    |
+   * |-----------------------------------------------------------------------------------------+
+   * | Shift     |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  | RSh |  U  | DEL |
+   * |-----------------------------------------------------------------------------------------+
+   * | Ctrl |  NO  |  Alt  |              Space                | RAlt | OS   |  L  |  D  |  R  |
+   * `-----------------------------------------------------------------------------------------'
+   */
+
+  LAYOUT_directional(
+      KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, ________, KC_BSPC,
+      KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
+      MO(_FN), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
+      KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_DEL,
+      KC_LCTL, MO(_FN), KC_LALT, KC_SPC, KC_SPC, KC_SPC, TD(TD_HOME), MO(_ST), KC_LEFT, KC_DOWN, KC_RIGHT
       ),
 
   /* FN (backtick/lower) Layer
@@ -41,12 +78,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 
   LAYOUT_directional(
-      KC_GRV, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, KC_F11, KC_F12, KC_TRNS, ______,
-      ______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, ______, ______, ______, ______, ______,
-      KC_CAPS, BL_TOGG, BL_STEP, BL_INC, BL_DEC, ______, ______, ______, ______, ______, ______, KC_MPLY,
-      ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, KC_VOLU, KC_MUTE,
-      ______, ______, ______, ______, ______, ______, ______, ______, KC_MPRV, KC_VOLD, KC_MNXT 
+      KC_CAPS,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, _______, _______,
+      _______, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+      _______, KC_LEFT, KC_DOWN, KC_RIGHT,_______, _______, _______, _______, _______, _______, _______, _______, KC_MPLY,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLU, KC_MUTE,
+      _______, _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_VOLD, KC_MNXT 
       ),
+
+        /* Setting / ADJUST Layer
+   * ,-----------------------------------------------------------------------------------------.
+   * | `   |  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  F7 |  F8 |  F9 | F10 | F11 | F12 |           |
+   * |-----------------------------------------------------------------------------------------+
+   * |        |RBB T|RGB M| Hue+| Hue-| Sat+| Sat-| Val+| Val-|     |     |      |      |      |
+   * |-----------------------------------------------------------------------------------------+
+   * |         | BL T| BL M| BL+ | BL- |     |     |     |     |     |     |     |             |
+   * |-----------------------------------------------------------------------------------------+
+   * |           |     |     |     |     |     |     |     |     |     |     |     |     |     |
+   * |-----------------------------------------------------------------------------------------+
+   * |      |       |       |                                   |     |      |     |     |     |
+   * `-----------------------------------------------------------------------------------------'
+   */
+
+  LAYOUT_directional(
+      KC_CAPS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, KC_F11, KC_F12, _______, RESET,
+      _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, _______, _______,
+      _______, BL_TOGG, BL_STEP, BL_INC,  BL_DEC, _______, _______, _______, _______, _______, _______,  _______, KC_MPLY,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, LGUI(KC_UP), LCTL(S(KC_ESC)),
+      _______, _______, _______, _______, _______, _______, _______, _______, LCTL(LGUI(KC_LEFT)), LGUI(KC_DOWN), LCTL(LGUI(KC_RIGHT)) 
+      )
+
 
 };
 
