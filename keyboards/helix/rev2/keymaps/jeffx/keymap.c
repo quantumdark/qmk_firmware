608b9462bece18dbee080f8dc5a5180d9dd40f0a
jeffreykxiao <jeffrey@ascent.ai>
2018-12-01 01:04:39 +0900

    Try something new + add extrakey flag

--- keyboards/helix/rev2/keymaps/jeffx/keymap.c
+++ keyboards/helix/rev2/keymaps/jeffx/keymap.c
@@ -38,8 +38,6 @@ enum custom_keycodes {
   RAISE,
   FUNC,
   ADJUST,
-  EISU,
-  KANA,
   BACKLIT,
   RGBRST
 };
@@ -63,72 +61,72 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * .-----------------------------------------------------.                      .-----------------------------------------------------.
  * | TAB    | Q      | W      | E      | R      | T      |                      | Y      | U      | I      | O      | P      | BACKSP |
  * |--------+--------+--------+--------+--------+--------|                      |--------+--------+--------+--------+-----------------|
- * | LCTRL    | A      | S      | D      | F      | G      |                      | H      | J      | K      | L      | ;      | ENTER    |
+ * | LCTRL  | A      | S      | D      | F      | G      |                      | H      | J      | K      | L      | ;      | ENTER  |
  * |--------+--------+--------+--------+--------+--------|                      |--------+--------+--------+-----------------+--------|
- * | LSHIFT  | Z      | X      | C      | V      | B      |                      | N      | M      | ,      | .      | /      | RSHIFT  |
+ * | LSHIFT | Z      | X      | C      | V      | B      |                      | N      | M      | ,      | .      | /      | RSHIFT |
  * |--------+--------+--------+--------+--------+--------+--------.    .--------+--------+--------+--------------------------+--------|
- * | LSHIFT | OS     | ALT    | EISU   | LOWER  | ASD    | SPACE  |    | LOWER  | RAISE  | KANA   | LEFT      | DOWN     | UP      | RSHIFT |
+ * | ESC    | OS     | ALT    | EISU   | LOWER  | ASD    | SPACE  |    | LOWER  | RAISE  | KANA   | LEFT   | DOWN   | UP     | RSHIFT |
  * '--------------------------------------------------------------'    '--------------------------------------------------------------'
  */
  [_QWERTY] = LAYOUT( \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,  \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                           KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-    KC_ESC,  _______, _______, KC_LGUI, KC_ALT,  LOWER,   KC_SPC,       LOWER,   RAISE,   FUNC,    _______, _______, _______, KC_DEL  \
+    _______, KC_LGUI, KC_LALT, _______, _______, FUNC,    KC_SPC,       LOWER,   RAISE,   KC_DEL,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
 ),
 
   /* LOWER
  * .-----------------------------------------------------.                      .-----------------------------------------------------.
- * | ESC    | 1      | 2      | 3      | 4      | 5      |                      | 6      | 7      | 8      | 9      | 10     | BACKSP |
+ * | TAB    | 1      | 2      | 3      | 4      | 5      |                      | 6      | 7      | 8      | 9      | 0      | BACKSP |
  * |--------+--------+--------+--------+--------+--------|                      |--------+--------+--------+--------+-----------------|
- * | TAB    | A      | S      | D      | F      | G      |                      | H      | J      | K      | L      | ;      | DEL    |
+ * | LCTRL  | A      | S      | D      | F      | G      |                      | H      | J      | K      | L      | ;      | ENTER  |
  * |--------+--------+--------+--------+--------+--------|                      |--------+--------+--------+-----------------+--------|
- * | LCTRL  | Z      | X      | C      | V      | B      |                      | N      | M      | ,      | .      | /      | ENTER  |
+ * | LSHIFT | Z      | X      | C      | V      | B      |                      | N      | M      | ,      | .      | /      | RSHIFT |
  * |--------+--------+--------+--------+--------+--------+--------.    .--------+--------+--------+--------------------------+--------|
- * | LSHIFT | OS     | ALT    | EISU   | LOWER  | ASD    | SPACE  |    | LOWER  | RAISE  | KANA   | ,      | .      | /      | RSHIFT |
+ * | ESC    | OS     | ALT    | EISU   | LOWER  | ASD    | SPACE  |    | LOWER  | RAISE  | KANA   | LEFT   | DOWN   | UP     | RSHIFT |
  * '--------------------------------------------------------------'    '--------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT( \
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-    _______, _______, _______, _______, _______, _______,                        _______, KC_MINS, KC_EQL,  _______, KC_QUOT, KC_MPLY, \
-    _______, _______, _______, _______, _______, _______,                        _______, _______, KC_LBRC, KC_RBRC, KC_BSLS, _______, \
+    _______, _______, _______, _______, _______, _______,                        KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT, _______, \
+    _______, _______, _______, _______, _______, _______,                        KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, _______, \
     _______, _______, _______, _______, _______, _______, _______,      _______, _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT \
 ),
 
-  /* LOWER
+  /* RAISE
  * .-----------------------------------------------------.                      .-----------------------------------------------------.
- * | ESC    | 1      | 2      | 3      | 4      | 5      |                      | 6      | 7      | 8      | 9      | 10     | BACKSP |
+ * | TAB    | !      | @      | #      | $      | %      |                      | ^      | &      | *      | (      | )      | BACKSP |
  * |--------+--------+--------+--------+--------+--------|                      |--------+--------+--------+--------+-----------------|
- * | TAB    | A      | S      | D      | F      | G      |                      | H      | J      | K      | L      | ;      | DEL    |
+ * | LCTRL  | A      | S      | D      | F      | G      |                      | H      | J      | K      | L      | ;      | ENTER  |
  * |--------+--------+--------+--------+--------+--------|                      |--------+--------+--------+-----------------+--------|
- * | LCTRL  | Z      | X      | C      | V      | B      |                      | N      | M      | ,      | .      | /      | ENTER  |
+ * | LSHIFT | Z      | X      | C      | V      | B      |                      | N      | M      | ,      | .      | /      | RSHIFT |
  * |--------+--------+--------+--------+--------+--------+--------.    .--------+--------+--------+--------------------------+--------|
- * | LSHIFT | OS     | ALT    | EISU   | LOWER  | ASD    | SPACE  |    | LOWER  | RAISE  | KANA   | ,      | .      | /      | RSHIFT |
+ * | ESC    | OS     | ALT    | EISU   | LOWER  | ASD    | SPACE  |    | LOWER  | RAISE  | KANA   | LEFT   | DOWN   | UP     | RSHIFT |
  * '--------------------------------------------------------------'    '--------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT( \
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                        KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
-    _______, _______, _______, _______, _______, _______,                        _______, KC_UNDS, KC_PLUS, _______, KC_DQUO, KC_MPLY, \
-    _______, _______, _______, _______, _______, _______,                        _______, _______, KC_LCBR, KC_RCBR, KC_PIPE, _______, \
+    _______, _______, _______, _______, _______, _______,                        KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_DQUO, _______, \
+    _______, _______, _______, _______, _______, _______,                        KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, _______, \
     _______, _______, _______, _______, _______, _______, _______,      _______, _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT \
 ),
 
-  /* LOWER
+  /* FUNC
  * .-----------------------------------------------------.                      .-----------------------------------------------------.
- * | ESC    | 1      | 2      | 3      | 4      | 5      |                      | 6      | 7      | 8      | 9      | 10     | BACKSP |
+ * | TAB    | F1     | F2     | F3     | F4     | F5     |                      | 6      | 7      | 8      | 9      | 0      | BACKSP |
  * |--------+--------+--------+--------+--------+--------|                      |--------+--------+--------+--------+-----------------|
- * | TAB    | A      | S      | D      | F      | G      |                      | H      | J      | K      | L      | ;      | DEL    |
+ * | LCTRL  | F6     | F7     | F8     | F9     | F10    |                      | LEFT   | DOWN   | UP     | RIGHT  | ;      | ENTER  |
  * |--------+--------+--------+--------+--------+--------|                      |--------+--------+--------+-----------------+--------|
- * | LCTRL  | Z      | X      | C      | V      | B      |                      | N      | M      | ,      | .      | /      | ENTER  |
+ * | LSHIFT | Z      | X      | C      | F11    | F12    |                      | N      | M      | ,      | .      | /      | RSHIFT |
  * |--------+--------+--------+--------+--------+--------+--------.    .--------+--------+--------+--------------------------+--------|
- * | LSHIFT | OS     | ALT    | EISU   | LOWER  | ASD    | SPACE  |    | LOWER  | RAISE  | KANA   | ,      | .      | /      | RSHIFT |
+ * | ESC    | OS     | ALT    | EISU   | LOWER  | ASD    | SPACE  |    | LOWER  | RAISE  | KANA   | LEFT   | DOWN   | UP     | RSHIFT |
  * '--------------------------------------------------------------'    '--------------------------------------------------------------'
  */
 [_FUNC] = LAYOUT( \
-    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                        KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
-    _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,                       KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_MPLY, \
-    _______, _______, _______, _______, KC_F11,  KC_F12,                       _______, _______, KC_LCBR, KC_RCBR, KC_PIPE, _______, \
-    _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT \
+    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                          _______, _______, _______, _______, _______, KC_MUTE, \
+    _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,                         KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, _______, KC_MPLY, \
+    _______, _______, _______, _______, KC_F11,  KC_F12,                         _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______,      _______, _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT \
 ),
   
   /* LOWER
@@ -143,10 +141,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * '--------------------------------------------------------------'    '--------------------------------------------------------------'
  */
 [_ADJUST] =  LAYOUT( \
-    _______, RESET,   _______, _______, _______, _______,                      _______, _______, _______, _______, _______, KC_DEL, \
-    _______, _______, _______, _______, _______, _______,                      _______, _______, RGBRST,  _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______,                      _______, _______, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, \
-    _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, RGB_SMOD,RGB_HUD, RGB_SAD, RGB_VAD \
+    _______, RESET,   _______, _______, _______, _______,                        _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______,                        _______, _______, RGBRST,  _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______,                        _______, _______, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, \
+    _______, _______, _______, _______, _______, _______, _______,      _______, _______, _______, RGB_SMOD,RGB_HUD, RGB_SAD, RGB_VAD \
 )
 };
 
@@ -177,12 +175,6 @@ void update_tri_layer_RGB(uint8_t layer1, uint8_t layer2, uint8_t layer3) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case QWERTY:
-      if (record->event.pressed) {
-        persistent_default_layer_set(1UL<<_QWERTY);
-      }
-      return false;
-      break;
     case LOWER:
       if (record->event.pressed) {
           //not sure how to have keyboard check mode and set it to a variable, so my work around
@@ -269,30 +261,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           RGB_current_mode = rgblight_config.mode;
         }
       #endif
-      return false;
-      break;
-    case EISU:
-      if (record->event.pressed) {
-        if(keymap_config.swap_lalt_lgui==false){
-          register_code(KC_LANG2);
-        }else{
-          SEND_STRING(SS_LALT("`"));
-        }
-      } else {
-        unregister_code(KC_LANG2);
-      }
-      return false;
-      break;
-    case KANA:
-      if (record->event.pressed) {
-        if(keymap_config.swap_lalt_lgui==false){
-          register_code(KC_LANG1);
-        }else{
-          SEND_STRING(SS_LALT("`"));
-        }
-      } else {
-        unregister_code(KC_LANG1);
-      }
       return false;
       break;
     case RGBRST:
