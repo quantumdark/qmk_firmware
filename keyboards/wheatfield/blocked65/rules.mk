07260d4733814ac85787e91075d03dd6661c92aa
jeffreykxiao <jeffrey@ascent.ai>
2019-04-21 00:38:44 +0900

    Quick update to get this working for my board

--- keyboards/wheatfield/blocked65/rules.mk
+++ keyboards/wheatfield/blocked65/rules.mk
@@ -57,4 +57,4 @@ MIDI_ENABLE = no            # MIDI controls
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
-TAP_DANCE_ENABLE = yes
+TAP_DANCE_ENABLE = no
